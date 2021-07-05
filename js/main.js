7382f524a45131807ecee0b40080576f5c142a24
trueotmor <proskurin.sasha78@gmail.com>
2021-07-05 11:00:27 +0300

    добавлено случайно количество комментариев в simple.js

--- js/main.js
+++ js/main.js
@@ -2,4 +2,4 @@ import {photo} from './data.js';
 import './sample.js';
 
 photo(25); // Введите желаемое количество фото
-// console.log(photo(21));
+console.log(photo(21));
--- js/sample.js
+++ js/sample.js
@@ -13,7 +13,7 @@ for (let idx = 0; idx < photos.length; idx++) {
   const copyTemplate = sampleTemplate.cloneNode(true);
   copyTemplate.querySelector('.picture__img').setAttribute('src', photos[idx].url);
   copyTemplate.querySelector('.picture__likes').textContent = photos[idx].likes;
-  copyTemplate.querySelector('.picture__comments').textContent = '1';
+  copyTemplate.querySelector('.picture__comments').textContent = photos[idx].comments.length;
   fragment.appendChild(copyTemplate);
 }
 
