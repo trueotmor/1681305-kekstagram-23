1d784e0d5d435eed0073866c10be14c50f5ee945
trueotmor <proskurin.sasha78@gmail.com>
2021-07-07 11:52:04 +0300

    module7-task2 на проверку

--- js/fullsize.js
+++ js/fullsize.js
@@ -1,14 +1,39 @@
-import { photos } from './sample.js';
 
 
-const bigPicture = document.querySelector('.big-picture');
-bigPicture.classList.remove('hidden');
+const getBigPicture = function (picture) {
+  const bigPicture = document.querySelector('.big-picture');
+  const bigPictureClose = bigPicture.querySelector('.big-picture__cancel');
+  const socialComments = document.querySelector('.social__comments');
+  const comments = picture.comments;
+  const newComment = comments.map( (comment) => `<li class="social__comment">
+    <img class="social__picture" src='${comment.avatar}' width="35" height="35">
+    <p class="social__text"> '${comment.message}' </p>
+    </li>`).join('');
+  socialComments.innerHTML = newComment;
 
+  function closePopupEvents () {
+    bigPicture.classList.add('hidden');
+    document.querySelector('body').classList.remove('modal-open');
+  }
 
-for (let idx = 0; idx < photos.length; idx++) {
-  bigPicture.querySelector('.big-picture__img img').setAttribute('src', photos[idx].url);
-  bigPicture.querySelector('.likes-count').textContent = photos[idx].likes;
-  bigPicture.querySelector('.comments-count').textContent = photos[idx].comments.length;
-}
+  bigPictureClose.addEventListener('click', ()=> {
+    closePopupEvents();
+  });
 
+  document.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape') {
+      closePopupEvents();
+    }
+  });
 
+  bigPicture.classList.remove('hidden');
+  bigPicture.querySelector('.social__comment-count').classList.add('hidden');
+  bigPicture.querySelector('.comments-loader').classList.add('hidden');
+  document.querySelector('body').classList.add('modal-open');
+  bigPicture.querySelector('.big-picture__img img').setAttribute('src', picture.url);
+  bigPicture.querySelector('.likes-count').textContent = picture.likes;
+  bigPicture.querySelector('.comments-count').textContent = picture.comments.length;
+  bigPicture.querySelector('.social__caption').textContent = picture.description;
+};
+
+export {getBigPicture};
--- js/sample.js
+++ js/sample.js
@@ -1,9 +1,11 @@
 import {photo} from './data.js';
+import { getBigPicture } from './fullsize.js';
 
 
 const sampleTemplate = document.querySelector('#picture').content;
 
 const picturesContainer = document.querySelector('.pictures');
+
 
 const MAX_PHOTOS_COUNT = 25; // Введите желаемое количество фото
 const photos = photo(MAX_PHOTOS_COUNT);
@@ -17,7 +19,15 @@ for (let idx = 0; idx < photos.length; idx++) {
   copyTemplate.querySelector('.picture__comments').textContent = photos[idx].comments.length;
   fragment.appendChild(copyTemplate);
 }
-
 picturesContainer.appendChild(fragment);
+
+const pictures = picturesContainer.querySelectorAll('.picture');
+
+for (let idx = 0; idx < pictures.length; idx++) {
+  const currentPhoto = photos[idx];
+  pictures[idx].addEventListener('click', ()=> {
+    getBigPicture(currentPhoto);
+  });
+}
 
 export {photos};
